1539f540f9d35848058e89345af8efee93de3537
Andy Bleach <ableach@siteset.co.uk>
2021-01-16 10:58:53 +0000

    open the file or return error message

--- filehandling.c
+++ filehandling.c
@@ -1,5 +1,14 @@
 #include <stdio.h>
 
 int main () {
+    FILE *p_file;
+
+    p_file = fopen("testdata.txt", "r");
+
+    if (p_file == NULL) {
+        printf("Unable to open file.\n");
+        return -1;
+    }
+
     return 0;
 }
