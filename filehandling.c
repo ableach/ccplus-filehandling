677b21e12ab311dbd33ccf9063a6223447527ff1
Andy Bleach <ableach@siteset.co.uk>
2021-01-16 11:06:04 +0000

    Su

--- filehandling.c
+++ filehandling.c
@@ -1,7 +1,13 @@
+/*
+Author: A Bleach
+Date: 16/01/2021
+*/
 #include <stdio.h>
 
 int main () {
     FILE *p_file;
+    char str[80];
+    int lineCount = 0;
 
     p_file = fopen("testdata.txt", "r");
 
@@ -9,6 +15,14 @@ int main () {
         printf("Unable to open file.\n");
         return -1;
     }
+
+    while ( fgets(str,81,p_file)!=NULL ) {
+        lineCount++;
+    } 
+
+    printf("File has %i lines\n", lineCount);
+
+    fclose(p_file);
 
     return 0;
 }
